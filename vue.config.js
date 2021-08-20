4b05ce5603e3c8e8286c6aa0179cbc73324fae9e
Sant Mac2 <sganis@gmail.com>
2021-08-20 14:44:54 +0300

    Added vue

---
+++ vue.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    pluginOptions: {
+        electronBuilder: {
+            mainProcessFile: 'src/main/background.js',
+            rendererProcessFile: 'src/renderer/index.js',
+            preload: 'src/main/preload.js', 
+            appId: "com.chaintrust.staker",
+            outputDir: "dist",
+            builderOptions: {
+                // asar: false,
+                win: {
+                    target: 'dir',
+                    icon: "src/resources/icons/icon.ico"
+                },
+                linux: {
+                    target: 'dir',
+                    category: 'System',
+                    icon: "src/resources/icons/icon.png"
+                },
+                mac: {
+                    target: 'dir',
+                    category: "public.app-category.developer-tools",
+                    icon: "src/resources/icons/icon.icns"
+                },
+                directories: {
+                    buildResources: "src/resources",
+                },
+                extraFiles: [
+                        "tool/bin/*","tool/config/*",                  
+                ]
+            }           
+        },        
+    },
+};
